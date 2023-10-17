cc29f21aaab662366093b7bec7d23c27aa112543
Casey Wittrock <caseywittrockmn@gmail.com>
2023-10-17 05:22:05 -0500

    finish assessment

--- snack-or-booze/src/App.js
+++ snack-or-booze/src/App.js
@@ -5,21 +5,35 @@ import Home from "./Home";
 import SnackOrBoozeApi from "./Api";
 import NavBar from "./NavBar";
 import { Route, Switch } from "react-router-dom";
-import Menu from "./FoodMenu";
-import Snack from "./FoodItem";
+import Menu from "./Menu";
+import Item from "./Item";
+import ItemForm from "./ItemForm";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [snacks, setSnacks] = useState([]);
+  const [drinks, setDrinks] = useState([]);
 
   useEffect(() => {
-    async function getSnacks() {
-      let snacks = await SnackOrBoozeApi.getSnacks();
+    async function getItems() {
+      let snacks = await SnackOrBoozeApi.getItems("snacks");
+      let drinks = await SnackOrBoozeApi.getItems("drinks");
+      setDrinks(drinks);
       setSnacks(snacks);
       setIsLoading(false);
     }
-    getSnacks();
+    getItems();
   }, []);
+
+  const handleAddItem = async (item) => {
+    let newItem = await SnackOrBoozeApi.addItem(item);
+
+    if (item.type === "snacks") {
+      setSnacks((snacks) => [...snacks, newItem]);
+    } else {
+      setDrinks((drinks) => [...drinks, newItem]);
+    }
+  };
 
   if (isLoading) {
     return <p>Loading &hellip;</p>;
@@ -32,16 +46,35 @@ function App() {
         <main>
           <Switch>
             <Route exact path="/">
-              <Home snacks={snacks} />
+              <Home snacks={snacks} drinks={drinks} />
             </Route>
             <Route exact path="/snacks">
-              <Menu snacks={snacks} title="Snacks" />
+              <Menu
+                items={snacks}
+                title="Snacks"
+                description="Yummy foods for your tummy"
+              />
             </Route>
-            <Route path="/snacks/:id">
-              <Snack items={snacks} cantFind="/snacks" />
+            <Route exact path="/snacks/:id">
+              <Item items={snacks} cantFind="/snacks" />
+            </Route>
+            <Route exact path="/drinks">
+              <Menu
+                items={drinks}
+                title="Drinks"
+                description="Delicious beverages for your thirst"
+              />
+            </Route>
+            <Route exact path="/drinks/:id">
+              <Item items={drinks} cantFind="/drinks" />
+            </Route>
+            <Route exact path="/add">
+              <ItemForm onSubmit={handleAddItem} isLoading={setIsLoading} />
             </Route>
             <Route>
-              <p>Hmmm. I can't seem to find what you want.</p>
+              <p className="not-found">
+                Hmmm. I can't seem to find what you want.
+              </p>
             </Route>
           </Switch>
         </main>
--- snack-or-booze/src/Home.js
+++ snack-or-booze/src/Home.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Card, CardBody, CardTitle } from "reactstrap";
 
-function Home() {
+function Home({ snacks, drinks }) {
   return (
     <section className="col-md-8">
       <Card>
         <CardBody className="text-center">
           <CardTitle>
-            <h3 className="font-weight-bold">
+            <span className="font-weight-bold">
               Welcome to Silicon Valley's premier dive cafe!
-            </h3>
+            </span>
+            <p>
+              We have {snacks.length} snacks, and {drinks.length} drinks.
+            </p>
           </CardTitle>
         </CardBody>
       </Card>
